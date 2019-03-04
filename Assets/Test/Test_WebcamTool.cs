240ab3e6f2d9e9e13205fbd683cbe16e76655a21
gim_razer <rgm79ms@gmail.com>
2019-03-04 12:46:20 +0800

    Added test scene

---
+++ Assets/Test/Test_WebcamTool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_WebcamTool : MonoBehaviour {
+
+    public WebcamTool webcamTool;
+    public Material targetMaterial;
+	
+	void Start () {
+
+        webcamTool = FindObjectOfType<WebcamTool>();
+        if (webcamTool)
+            webcamTool.Init();
+        else
+            Debug.LogError("WebcamUtility Not Found");
+
+        targetMaterial.mainTexture = webcamTool.GetWebcamTex();
+    }
+	
+	void Update () {
+		
+	}
+}
